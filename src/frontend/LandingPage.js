e998af665c4d10cd7a5a6c49341bc64db8852efe
tewarimohit <tewarimohit955@gmail.com>
2022-07-13 01:10:30 +0530

    feat:working on landing page

--- src/frontend/LandingPage.js
+++ src/frontend/LandingPage.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "../frontend/styles/LandingPage.css";
 import landing from "../assets/landing-pic.svg";
 
 const LandingPage = () => {
 	return (
 		<div>
-			<div className="home-img">
-				<img src={landing} alt="home-image" />
+			<div className="">
+				<h1 className="hd-l-font text-center margin-2">Habit Tracker</h1>
+				<div className="landing-img d-flex  ">
+					<div className="width-50">
+						<p className="ml-2 hd-s-font">Meet Your Modern</p>
+						<p className="ml-2 dark-text hd-s-font">Habit Tracker app</p>
+					</div>
+					<img src={landing} alt="landing-image" />
+				</div>
 			</div>
 		</div>
 	);
