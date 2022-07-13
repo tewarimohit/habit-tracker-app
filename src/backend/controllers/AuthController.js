32fd15117c6a421bf6105769d8831bba5d4b1651
Mohit Tewari <tewarimohit955@gmail.com>
2022-07-13 19:14:01 +0000

    feat: added home page ui

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import { v4 as uuid } from "uuid";
 import { Response } from "miragejs";
 import { formatDate } from "../utils/authUtils";
+import { requiresAuth } from "../utils/authUtils";
 const sign = require("jwt-encode");
 
 /**
